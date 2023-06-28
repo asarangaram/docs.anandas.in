d2338712d7598300b7106eb1fd50f7d5b2a42245
Ananda Sarangaram (Desktop) <4833102+asarangaram@users.noreply.github.com>
2023-06-28 20:31:13 +0530

    Moving to md files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Image Repo'
+copyright = '2021, anandas.in'
+author = 'Ananda S'
 
 release = '0.1'
 version = '0.1.0'
